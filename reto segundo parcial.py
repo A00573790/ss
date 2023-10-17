8cbfb8725b6239fc70e3cb6a60d498eb3bc86ed2
A00573790 <145025715+A00573790@users.noreply.github.com>
2023-10-17 07:29:41 -0600

    Create reto segundo parcial.py

---
+++ reto segundo parcial.py
@@ -0,0 +1,9 @@
+
+puntuacion_examen=int(input("inserte la puntuacion de se examen: "))
+numero_asignaciones=int(input("inserte numero de asignaciones completadas: "))
+if puntuacion_examen >= 70 and numero_asignaciones >= 5:
+  print("felicidades aprobo")
+participacion=int(input("participacion S/N"))
+if puntuacion_examen >= 70 and participacion:
+  print("felicidades aprobo")
+
